1f64745aac9095e9033f56b31eb1b482c432b4dd
Rafal Wolny <rafal.wolny96@gmail.com>
2019-10-21 11:05:12 +0200

    Updates

---
+++ precache-manifest.5c500fe2d915c404b002d89f7ff4e9b6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7146e9f9630525227e939be256e924ad",
+    "url": "/landing-page/index.html"
+  },
+  {
+    "revision": "ebaf980c82aa69fdfcd8",
+    "url": "/landing-page/static/css/main.e944fa34.chunk.css"
+  },
+  {
+    "revision": "c326ea42e723ce92e3be",
+    "url": "/landing-page/static/js/2.e1be74df.chunk.js"
+  },
+  {
+    "revision": "ebaf980c82aa69fdfcd8",
+    "url": "/landing-page/static/js/main.c44ffad0.chunk.js"
+  },
+  {
+    "revision": "47c74ace8ec716c8cfb6",
+    "url": "/landing-page/static/js/runtime~main.bc0aaa2c.js"
+  },
+  {
+    "revision": "969ed64085ed6c9a6f28b143a0379a2c",
+    "url": "/landing-page/static/media/background.969ed640.jpg"
+  }
+]);
